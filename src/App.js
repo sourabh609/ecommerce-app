4455d704a5e5b8205478ae078dadf304e01774e1
Sourabh Mishra <sourabh.mishra609@gmail.com>
2021-08-03 16:57:15 +0530

    commit for basic home page

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
-import React, { Component } from 'react';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
+import HomePage from "./Homepage.component.jsx";
 
 class App extends Component {
   render() {
-    return <div className='App'>Hello World</div>;
+    return (
+      <div className="App">
+        <HomePage />
+      </div>
+    );
   }
 }
 
---
+++ src/Homepage.component.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import './homepage.styles.scss';
+
+const Homepage = () => (
+  <div className="homepage">
+    <div className="directory-menu">
+      <div className="menu-item">
+        <div classname="content">
+          <h1 className="title">Hats</h1>
+          <span classname="subtitle">Shop Now</span>
+        </div>
+      </div>
+    </div>
+  </div>
+);
+
+export default Homepage;
