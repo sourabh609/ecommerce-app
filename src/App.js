22d440dd5d2e026202514042ab402bbfd32099af
Sourabh Mishra <sourabh.mishra609@gmail.com>
2021-08-05 15:20:03 +0530

    commit for shop page

--- src/App.js
+++ src/App.js
@@ -2,18 +2,17 @@ import React, { Component } from "react";
 import "./App.css";
 import HomePage from "./pages/homepage/HomePage.component.jsx";
 import { Switch, Route } from "react-router-dom";
-
-const HatsPage = () => {
-  return <h1>Hats Page</h1>;
-};
+import { ShopPage } from "./pages/shopPage/ShopPage.component";
+import Header from "./components/header/header.component";
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
+      <div>
+        <Header />
         <Switch>
-          <Route exact path="/" component={HomePage}/>
-          <Route exact path="/shop/hats" component={HatsPage}/>
+          <Route exact path="/" component={HomePage} />
+          <Route exact path="/shop" component={ShopPage} />
         </Switch>
       </div>
     );
---
+++ src/pages/shopPage/ShopPage.component.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import SHOP_DATA from "./shopData.js";
+import CollectionPreview from "../../components/collection-preview/CollectionPreview.component";
+
+export class ShopPage extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      collections: SHOP_DATA,
+    };
+  }
+
+  render() {
+    const { collections } = this.state;
+    return (
+      <div>
+        {collections.map(({ id, ...otherCollectionProps }) => (
+          <CollectionPreview key={id} {...otherCollectionProps} />
+        ))}
+      </div>
+    );
+  }
+}
